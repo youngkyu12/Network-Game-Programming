c5b1ce671de2447f95c78444df40b8075bdb8325
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-01 18:03:18 +0900

    윈속 초기화, 종료

--- NetworkProj_Server/NetworkProj_Server.cpp
+++ NetworkProj_Server/NetworkProj_Server.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// 윈속 초기화
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return 1;
+	}
+
+	//윈속 종료
+	WSACleanup();
 }
 
 
