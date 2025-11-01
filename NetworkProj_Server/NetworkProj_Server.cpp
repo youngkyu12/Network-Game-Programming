704b9ca730a0c4c07570bf27a1ac52bf17c21311
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-01 18:17:51 +0900

    bind,listen,accept 추가

--- NetworkProj_Server/NetworkProj_Server.cpp
+++ NetworkProj_Server/NetworkProj_Server.cpp
@@ -3,6 +3,8 @@
 #include <ws2tcpip.h>
 using namespace std;
 
+#define SERVERPORT 8922
+
 
 int main()
 {
@@ -11,6 +13,44 @@ int main()
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
 		return 1;
+	}
+	// 소켓 생성
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		return 1;
+	}
+
+	// 주소, 포트 입력
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(SERVERPORT);
+
+	// bind
+	if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		return 1;
+	}
+
+	// listen
+	if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		return 1;
+	}
+
+	while (true)
+	{
+		// 접속 시작
+		SOCKADDR_IN clientAddr;
+		int addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			return 1;
+		}
+
 	}
 
 	//윈속 종료
