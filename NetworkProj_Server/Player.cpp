fc63c3b9da1079a0f46c639df70522e7f652a3c9
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-20 14:36:25 +0900

    Merge branch 'main' into Client

---
+++ NetworkProj_Server/Player.cpp
@@ -0,0 +1,102 @@
+#include "Player.h"
+
+Player::Player()
+{
+
+}
+Player::~Player()
+{
+
+}
+
+void Player::SetPosition(float x, float y, float z)
+{
+	Position = XMFLOAT3(x, y, z);
+}
+
+void Player::SetLook(float x, float y, float z)
+{
+	Look = XMFLOAT3(x, y, z);
+	LookAt(Look, Up);
+}
+
+void Player::SetFireFlag(bool fireflag)
+{
+	Fire_Flag = fireflag;
+}
+
+void Player::LookAt(XMFLOAT3& L, XMFLOAT3& U)
+{
+	XMFLOAT4X4 xmf4x4View = Matrix4x4::LookAtLH(Position, L, U);
+	Right = Vector3::Normalize(XMFLOAT3(xmf4x4View._11, xmf4x4View._21, xmf4x4View._31));
+	Up = Vector3::Normalize(XMFLOAT3(xmf4x4View._12, xmf4x4View._22, xmf4x4View._32));
+	Look = Vector3::Normalize(XMFLOAT3(xmf4x4View._13, xmf4x4View._23, xmf4x4View._33));
+}
+
+XMFLOAT3 Player::GetPosition()
+{
+	return Position;
+}
+
+XMFLOAT3 Player::GetVelocity()
+{
+	return Velocity;
+}
+
+XMFLOAT3 Player::GetLook()
+{
+	return Look;
+}
+
+XMFLOAT3 Player::GetUp()
+{
+	return Up;
+}
+
+XMFLOAT3 Player::GetRight()
+{
+	return Right;
+}
+
+uint16_t Player::GetHP()
+{
+	return HP;
+}
+
+bool Player::GetFireFlag()
+{
+	return Fire_Flag;
+}
+
+
+void Player::Move(XMFLOAT3& xmf3Shift)
+{
+
+	Position = Vector3::Add(xmf3Shift, Position);
+}
+
+void Player::Rotate(float fPitch, float fYaw, float fRoll)
+{
+	if (fPitch != 0.0f)
+	{
+		XMMATRIX mtxRotate = XMMatrixRotationAxis(XMLoadFloat3(&Right), XMConvertToRadians(fPitch));
+		Look = Vector3::TransformNormal(Look, mtxRotate);
+		Up = Vector3::TransformNormal(Up, mtxRotate);
+	}
+	if (fYaw != 0.0f)
+	{
+		XMMATRIX mtxRotate = XMMatrixRotationAxis(XMLoadFloat3(&Up), XMConvertToRadians(fYaw));
+		Look = Vector3::TransformNormal(Look, mtxRotate);
+		Right = Vector3::TransformNormal(Right, mtxRotate);
+	}
+	if (fRoll != 0.0f)
+	{
+		XMMATRIX mtxRotate = XMMatrixRotationAxis(XMLoadFloat3(&Look), XMConvertToRadians(fRoll));
+		Up = Vector3::TransformNormal(Up, mtxRotate);
+		Right = Vector3::TransformNormal(Right, mtxRotate);
+	}
+
+	Look = Vector3::Normalize(Look);
+	Right = Vector3::Normalize(Vector3::CrossProduct(Up, Look));
+	Up = Vector3::Normalize(Vector3::CrossProduct(Look, Right));
+}
